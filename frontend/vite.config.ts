6733e109b0681b03c943d3c726e53ed04d283fc0
Eugene Didenko <eugeneddenko@gmail.com>
2025-05-31 23:41:28 +0200

    feat: add Docker and Makefile setup, configure frontend and backend services

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',
+        changeOrigin: true,
+      },
+    },
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5445,
+    },
+  },
 });
